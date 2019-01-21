e80109533eb8965bfd6875d0ca51376ac0ee46bd
rony <mysiterony@gmail.com>
2019-01-22 00:08:01 +0600

    display all question

---
+++ resources/views/question/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header">
+                    <div class="d-flex align-items-center"|>
+                            <h2>All Question</h2>
+                            <div class="ml-auto"|>
+                                   <a class="btn btn-outline-secondary" href="{{route('questions.create')}}"> Ask Question </a>
+                            </div>
+                    </div>
+                    
+                </div>
+
+                <div class="card-body">
+                    @foreach($questions as $question)
+                        <div class="media">
+                            <div class="d-flex flex-column counters" >
+                                <div class="vote">
+                                    <strong>{{$question->votes}}</strong> {{str_plural('vote','$question->votes')}}
+                                </div>
+                                <div class="status {{$question->status}}">
+                                        <strong>{{$question->answers}}</strong> {{str_plural('answer','$question->answers')}}
+                                </div>
+                                <div class="view">
+                                        {{$question->views ." ". str_plural('view','$question->views')}}
+                                </div>
+                            </div>
+                            <div class="media-body">
+                             <h3 class="mt-0"><a href="{{$question->url}}">{{$question->title}}</a></h3>
+                             Asked by<br/>
+                             <p class="lead"><a href="{{$question->user->url}}">{{$question->user->name}}</a><p>   
+                                <small class="text-muted">{{$question->created_at->diffForHumans()}}</small><br/>
+                                {{str_limit($question->body)}}
+                            </div>
+                        </div>
+                        <hr/>
+                    @endforeach
+                    <div class="mx-auto">
+                         {{$questions->links()}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
